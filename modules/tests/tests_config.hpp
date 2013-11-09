4032f385a8399a5424c6407eefc31c4fd78eb1b1
Paul Sastrasinh <psastras@gmail.com>
2013-11-09 12:14:22 -0500

    Delete tests_config.hpp

--- modules/tests/tests_config.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-/// This header is automatically generated.  Do not modify.
-/// Contains information about datasets for running tests. 
-
-const static std::string s_simple_data_dir 				= "/home/psastras/vocabtree/data/simple/";
-const static std::string s_simple_database_location 	= "/home/psastras/vocabtree/data/simple/db_simple.bin";
-
