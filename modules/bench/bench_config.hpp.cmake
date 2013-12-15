26795de5fb79e8660304a7efcbf269525cd2e828
Paul Sastrasinh <psastras@gmail.com>
2013-12-15 16:11:23 -0500

    added full feature feature precomp

--- modules/bench/bench_config.hpp.cmake
+++ modules/bench/bench_config.hpp.cmake
@@ -9,7 +9,7 @@ const static std::string s_simple_database_location 	= "@CMAKE_SOURCE_DIR@/data/
 
 const static std::string s_oxford_data_dir 				= "@CMAKE_SOURCE_DIR@/data/oxford/";
 const static std::string s_oxford_database_location 	= "@CMAKE_SOURCE_DIR@/data/oxford/db_oxford.bin";
-const static int s_oxford_num_clusters					= 32768;
+const static int s_oxford_num_clusters					= 65536;
 
 const static std::string s_oxford100k_data_dir 				= "@CMAKE_SOURCE_DIR@/data/oxford100k/";
 const static std::string s_oxford100k_database_location 	= "@CMAKE_SOURCE_DIR@/data/oxford100k/db_oxford100k.bin";
